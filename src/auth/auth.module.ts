360ae50349b53310acf8655d5b6578c73f6c967e
KyanD <dangtrungkien96@gmail.com>
2023-02-21 14:22:30 +0700

    fixed expired time

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,25 +1,24 @@
-import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { MailModule } from '../mail/mail.module';
-import { MailProcessor } from '../mail/mail.processor';
-import { UserModule } from '../user/user.module';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { AccountLogin } from './entities/account.entities';
-import { FacebookStragery } from './login_auth/facebook.stategies';
-import { GoogleStrategy } from './login_auth/google.stategies';
-import { JwtStrategy } from './login_auth/jwt.stategies';
-import { RefreshTokenStrategy } from './login_auth/jwtRefreshToken.stategies';
+import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { MailModule } from "../mail/mail.module";
+import { MailProcessor } from "../mail/mail.processor";
+import { UserModule } from "../user/user.module";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { AccountLogin } from "./entities/account.entities";
+import { FacebookStragery } from "./login_auth/facebook.stategies";
+import { GoogleStrategy } from "./login_auth/google.stategies";
+import { JwtStrategy } from "./login_auth/jwt.stategies";
+import { RefreshTokenStrategy } from "./login_auth/jwtRefreshToken.stategies";
 
 @Module({
   imports: [
     UserModule,
     PassportModule,
     JwtModule.register({
-      secret: 'hehe',
-      signOptions: { expiresIn: '300s' },
+      secret: "hehe",
     }),
     TypeOrmModule.forFeature([AccountLogin]),
     MailModule,
